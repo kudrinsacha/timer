dddc6f7b4ceb4380d7262992e7a82b0d3d8e607f
Alexandr Kudrin <8096304@gmail.com>
2024-04-16 22:01:00 +0300

    minor fix

--- src/hooks/useSizeProgressBar.ts
+++ src/hooks/useSizeProgressBar.ts
@@ -3,13 +3,13 @@ import { useMemo } from 'react';
 export const useSizeProgressBar = (time: number, isPlay: boolean) => {
     return useMemo(() => {
         if (isPlay) {
-            if (time > 5_940_000) {
+            if (time >= 6_000_000) {
                 return 290;
             } else {
                 return 270;
             }
         } else {
-            if (time > 5_940_000) {
+            if (time > 6_000_000) {
                 return 220;
             } else {
                 return 200;
